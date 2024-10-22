f0ac4358f0d4eeb62a6b41707d0da6d93429c18c
billyandryus <billyandrys@gmail.com>
2024-10-21 23:19:32 -0500

    add comments explacative

--- algorithms/exercises1/numbersPrime.ts
+++ algorithms/exercises1/numbersPrime.ts
@@ -1,19 +1,26 @@
-// valide is number prime
+// validate is number prime
 const isPrime = (numberValide: number ): boolean => {
     
     if( numberValide <= 1) return false
+    //validate if number is equal to or less than one
+
        for( let i = 2; i <= Math.sqrt( numberValide ); i++){
-            if( numberValide % i === 0 ){
+        // We make a cycle from 2 to the square root of the number  
+        // to be validated, to make ir more efficient
+
+
+        if( numberValide % i === 0 ){
+            //if the number to be validated is divisable by varible
+            //the cycle variable number, it means that it is not prime
                 return false
         } 
         
     } 
     
     return true
-}
-
-console.log(isPrime(5))
-console.log(isPrime(12))
+    // is no divisors are found, it means that it is prime
+    // return true 
+} 
 
 export {
     isPrime
--- algorithms/exercises1/test/exercises1.test.js
+++ algorithms/exercises1/test/exercises1.test.js
@@ -5,5 +5,6 @@ const { isPrime } = require('../numbersPrime')
 it('valide is number Prime', ()=>{
     expect(isPrime(5)).toBe(true)
     expect(isPrime(6)).toBe(false)
+    expect(isPrime(1)).toBe(false)
 })
 
